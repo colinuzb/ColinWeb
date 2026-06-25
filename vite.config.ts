cd499606133d4c04e7f413aa1e32bdc48b73e4d8
Colin <colinuzb@gmail.com>
2026-06-25 15:00:19 +0000

    ColinWeb

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/colinweb-portfolio/',
   plugins: [react()],
+  base: '/ColinWeb/',
 })
